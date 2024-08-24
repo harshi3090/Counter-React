4caded254811b25b66cd43a8742bf1a11ee54d88
Harshitha S <129758523+harshi3090@users.noreply.github.com>
2024-08-24 09:13:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+
+//import { use } from 'react';
+import './App.css';
+import { useState } from 'react';
+function App() {
+  let [count,setCount]=useState(0);
+       const result=()=>{
+       setCount(count=count+1);
+        };
+  const dec=()=>{
+    setCount(count=count-1);
+  };
+
+const zero=()=>{
+  setCount(count=0);
+}
+
+ return (
+  <div className="App">
+   <button class="inc" onClick={result}>Increase</button>
+   <button class="dec" onClick={dec}>Decrease</button>
+   <button  class="er" onClick={zero}>SetTOzero</button>
+   {count}
+
+  </div>
+ );
+}
+ 
+
+
+
+
+
+export default App;
